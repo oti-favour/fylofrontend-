05f366d98dff78c84cc2c332d43ee88583a2cb79
oti-favour <favourgraceoti@gmail.com>
2024-09-11 19:35:15 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        teal: {
+          400: '#64d8cb',
+        },
+      },
+     
+    },
+  },
+  plugins: [],
+}
